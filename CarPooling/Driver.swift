d39c53ef894f0e9c2d780c8b037196fddec5e0f7
Alessandro Luigi Marotta <aleluigimar@gmail.com>
2017-06-30 18:01:58 +0200

    Classe driver iniziata.

---
+++ CarPooling/Driver.swift
@@ -0,0 +1,29 @@
+//
+//  Driver.swift
+//  CarPooling
+//
+//  Created by Alessandro Luigi Marotta on 30/06/2017.
+//  Copyright © 2017 Andrea Tofano. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+import MapKit
+
+
+class Driver {
+    
+    
+    let name: String
+    let id: String
+    let phone: String?
+    let image: UIImage?
+    let ranking: Double?
+    
+    
+    
+    
+    
+    
+    
+}
