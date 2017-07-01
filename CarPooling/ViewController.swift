c45e281849f3aa24e886d613ffd7a81db3be0b5b
Andrea Tofano <tofanoandrea@gmail.com>
2017-07-01 15:12:34 +0200

    map

--- CarPooling/ViewController.swift
+++ CarPooling/ViewController.swift
@@ -13,16 +13,62 @@ import CoreLocation
 class ViewController: UIViewController {
     fileprivate var POIS = [Place]()
     fileprivate let locationManager = CLLocationManager()
+    let start = CLLocationCoordinate2D(latitude: 40.836087, longitude: 14.305694)
+    let arrival = CLLocationCoordinate2D(latitude: 40.847360, longitude: 14.281893)
+    let wp = CLLocationCoordinate2D(latitude: 40.837102 , longitude: 14.301563)
+    let wp2 = CLLocationCoordinate2D(latitude: 40.837102 , longitude: 14.331563)
     @IBOutlet weak var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate=self
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.startUpdatingLocation()
         locationManager.requestWhenInUseAuthorization()
         mapView.setUserTrackingMode(MKUserTrackingMode.follow, animated: false)
-
+        makeTravel()
+    }
+    
+    func makeTravel(){
+        let travel = [start,wp,wp2,arrival,arrival]
+        for k in 0..<travel.count-1 {
+            self.getDirections(start: travel[k], arrival: travel[k+1])
+            let point = PlaceAnnotation(location: travel[k], title: "\(k)", type: "paologay")
+            mapView.addAnnotation(point)
+        }
+        let point = PlaceAnnotation(location: travel.last!, title: "end", type: "cftvgybh")
+        mapView.addAnnotation(point)
+    }
+    
+    func getDirections(start : CLLocationCoordinate2D, arrival : CLLocationCoordinate2D) {
+        let request = MKDirectionsRequest()
+        request.source = MKMapItem(placemark: MKPlacemark(coordinate: start))
+        request.destination = MKMapItem(placemark: MKPlacemark(coordinate: wp))
+        request.requestsAlternateRoutes = false
+        request.transportType = .walking
+        let directions = MKDirections(request: request)
+        directions.calculate(completionHandler: {(response, error) in
+            
+            if error != nil {
+                print("Error getting directions")
+            } else {
+                self.showRoute(response!)
+            }
+        })
+    }
+    
+    func showRoute(_ response: MKDirectionsResponse) {
+        
+        for route in response.routes {
+            
+            mapView.add(route.polyline,
+                        level: MKOverlayLevel.aboveRoads)
+            
+            for step in route.steps {
+            }
+        }
+        self.mapView.showAnnotations(self.mapView.annotations, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,6 +86,48 @@ class ViewController: UIViewController {
     }
  */
 }
+
+func routeUrlCreate(_ departure : CLLocationCoordinate2D,_ arrival : CLLocationCoordinate2D , _ waypoints : [CLLocationCoordinate2D]) -> String{
+    var basicUrl = "https://maps.googleapis.com/maps/api/directions/json?origin=\(departure.latitude),\(departure.longitude)&destination=\(arrival.latitude),\(arrival.longitude)&key=AIzaSyAGjTDcNLXG5OVuluxG3MvSNiMbDJMUlns"
+    if waypoints.count != 0 {
+        basicUrl =
+        "https://maps.googleapis.com/maps/api/directions/json?origin=\(departure.latitude),\(departure.longitude)&destination=\(arrival.latitude),\(arrival.longitude)&waypoints=optimize:true"
+        for wp in waypoints {
+            basicUrl.append("|\(wp.latitude),\(wp.longitude)")
+        }
+        basicUrl.append("&key=AIzaSyAGjTDcNLXG5OVuluxG3MvSNiMbDJMUlns")
+    }
+    return basicUrl
+}
+
+func load(departure : CLLocationCoordinate2D, arrival : CLLocationCoordinate2D , waypoints : [CLLocationCoordinate2D], handler: @escaping (NSDictionary?, NSError?) -> Void){
+    let url = routeUrlCreate(departure,arrival,waypoints)
+    let session = URLSession(configuration: URLSessionConfiguration.default)
+    let dataTask = session.dataTask(with: URL(string: url)!) { data, response, error in
+        if let error = error {
+            print(error)
+        } else if let httpResponse = response as? HTTPURLResponse {
+            if httpResponse.statusCode == 200 {
+                
+                do {
+                    let responseObject = try JSONSerialization.jsonObject(with: data!, options: .allowFragments)
+                    guard let responseDict = responseObject as? NSDictionary else {
+                        return
+                    }
+                    
+                    handler(responseDict, nil)
+                    
+                } catch let error as NSError {
+                    handler(nil, error)
+                }
+            }
+        }
+    }
+    
+    dataTask.resume()
+
+}
+
 
 
 extension ViewController: CLLocationManagerDelegate {
@@ -99,8 +187,9 @@ extension ViewController: CLLocationManagerDelegate {
 extension ViewController: MKMapViewDelegate {
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
         let renderer = MKPolylineRenderer(overlay: overlay)
-        renderer.strokeColor = UIColor.red
-        renderer.lineWidth = 4.0
+        let colors = [UIColor.blue,UIColor.red,UIColor.cyan,UIColor.brown]
+        renderer.strokeColor = colors[Int(arc4random()) % colors.count]
+        renderer.lineWidth = 2.0
         
         return renderer
     }
@@ -109,17 +198,17 @@ extension ViewController: MKMapViewDelegate {
         let pin = MKAnnotationView(annotation: annotation, reuseIdentifier: nil)
         pin.canShowCallout=true
         var rect = CGSize(width: 30, height: 30)
-        var img = #imageLiteral(resourceName: "pin_city")
+        var img = #imageLiteral(resourceName: "paolo")
         if(annotation.title??.contains("plane"))!{
-            img = #imageLiteral(resourceName: "airplane")
+            img = #imageLiteral(resourceName: "paolo")
             rect = CGSize(width: 50, height: 50)
         }else if let t = annotation.subtitle{
             if let type = t {
                 if(type.contains("park")){
-                    img = #imageLiteral(resourceName: "pin_parchiNaturali")
+                    img = #imageLiteral(resourceName: "paolo")
                 }
                 else if(type.contains("monument")){
-                    img = #imageLiteral(resourceName: "pin_monumento")
+                    img = #imageLiteral(resourceName: "paolo")
                 }
             }
         }
